33c40cf0cfa673361565e7e5f648323a5f611096
KatarzynaKreja <78622515+KatarzynaKreja@users.noreply.github.com>
2022-10-23 21:29:14 +0200

    adding function for drop down list

--- js/index.js
+++ js/index.js
@@ -1,26 +1,50 @@
 function updateTimes() {
   let cataniaElement = document.querySelector("#catania");
-  let cataniaTimeElement = cataniaElement.querySelector(".time");
-  let cataniaDateElement = cataniaElement.querySelector(".date");
-  let cataniaTimezone = moment().tz("Europe/Rome");
+  if (cataniaElement) {
+    let cataniaTimeElement = cataniaElement.querySelector(".time");
+    let cataniaDateElement = cataniaElement.querySelector(".date");
+    let cataniaTimezone = moment().tz("Europe/Rome");
+    cataniaDateElement.innerHTML = cataniaTimezone.format("MMMM	Do YYYY");
+    cataniaTimeElement.innerHTML = cataniaTimezone.format("HH:mm:ss");
+  }
 
   let cracowElement = document.querySelector("#cracow");
-  let cracowTimeElement = cracowElement.querySelector(".time");
-  let cracowDateElement = cracowElement.querySelector(".date");
-  let cracowTimezone = moment().tz("Europe/Warsaw");
+  if (cracowElement) {
+    let cracowTimeElement = cracowElement.querySelector(".time");
+    let cracowDateElement = cracowElement.querySelector(".date");
+    let cracowTimezone = moment().tz("Europe/Warsaw");
+    cracowDateElement.innerHTML = cracowTimezone.format("MMMM	Do YYYY");
+    cracowTimeElement.innerHTML = cracowTimezone.format("HH:mm:ss");
+  }
 
   let hongkongElement = document.querySelector("#hongkong");
-  let hongkongTimeElement = hongkongElement.querySelector(".time");
-  let hongkongDateElement = hongkongElement.querySelector(".date");
-  let hongkongTimezone = moment().tz("Hongkong");
+  if (hongkongElement) {
+    let hongkongTimeElement = hongkongElement.querySelector(".time");
+    let hongkongDateElement = hongkongElement.querySelector(".date");
+    let hongkongTimezone = moment().tz("Hongkong");
+    hongkongDateElement.innerHTML = hongkongTimezone.format("MMMM	Do YYYY");
+    hongkongTimeElement.innerHTML = hongkongTimezone.format("HH:mm:ss");
+  }
+}
 
-  hongkongDateElement.innerHTML = hongkongTimezone.format("MMMM	Do YYYY");
-  cataniaDateElement.innerHTML = cataniaTimezone.format("MMMM	Do YYYY");
-  cracowDateElement.innerHTML = cracowTimezone.format("MMMM	Do YYYY");
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+          <div class="city" >
+          <div>
+            <h3>${cityName}</h3>
+            <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+          </div>
 
-  hongkongTimeElement.innerHTML = hongkongTimezone.format("HH:mm:ss");
-  cataniaTimeElement.innerHTML = cataniaTimezone.format("HH:mm:ss");
-  cracowTimeElement.innerHTML = cracowTimezone.format("HH:mm:ss");
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        </div>
+  `;
 }
 updateTimes();
 setInterval(updateTimes, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
