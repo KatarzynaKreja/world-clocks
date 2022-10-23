d904bc9545b2518719b83262f5fec91bf52e946c
KatarzynaKreja <78622515+KatarzynaKreja@users.noreply.github.com>
2022-10-23 20:54:04 +0200

    changing locations

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 function updateTimes() {
-  let romeElement = document.querySelector("#rome");
-  let romeTimeElement = romeElement.querySelector(".time");
-  let romeDateElement = romeElement.querySelector(".date");
-  let romeTimezone = moment().tz("Europe/Rome");
+  let cataniaElement = document.querySelector("#catania");
+  let cataniaTimeElement = cataniaElement.querySelector(".time");
+  let cataniaDateElement = cataniaElement.querySelector(".date");
+  let cataniaTimezone = moment().tz("Europe/Rome");
 
-  let warsawElement = document.querySelector("#warsaw");
-  let warsawTimeElement = warsawElement.querySelector(".time");
-  let warsawDateElement = warsawElement.querySelector(".date");
-  let warsawTimezone = moment().tz("Europe/Warsaw");
+  let cracowElement = document.querySelector("#cracow");
+  let cracowTimeElement = cracowElement.querySelector(".time");
+  let cracowDateElement = cracowElement.querySelector(".date");
+  let cracowTimezone = moment().tz("Europe/Warsaw");
 
   let hongkongElement = document.querySelector("#hongkong");
   let hongkongTimeElement = hongkongElement.querySelector(".time");
   let hongkongDateElement = hongkongElement.querySelector(".date");
   let hongkongTimezone = moment().tz("Hongkong");
 
-  romeDateElement.innerHTML = romeTimezone.format("MMMM	Do YYYY");
-  warsawDateElement.innerHTML = warsawTimezone.format("MMMM	Do YYYY");
   hongkongDateElement.innerHTML = hongkongTimezone.format("MMMM	Do YYYY");
+  cataniaDateElement.innerHTML = cataniaTimezone.format("MMMM	Do YYYY");
+  cracowDateElement.innerHTML = cracowTimezone.format("MMMM	Do YYYY");
 
-  romeTimeElement.innerHTML = romeTimezone.format("HH:mm:ss");
-  warsawTimeElement.innerHTML = warsawTimezone.format("HH:mm:ss");
   hongkongTimeElement.innerHTML = hongkongTimezone.format("HH:mm:ss");
+  cataniaTimeElement.innerHTML = cataniaTimezone.format("HH:mm:ss");
+  cracowTimeElement.innerHTML = cracowTimezone.format("HH:mm:ss");
 }
 updateTimes();
 setInterval(updateTimes, 1000);
